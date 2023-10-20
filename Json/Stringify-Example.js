9a7c535b5532171612c7a25a2366ecd2d1c23aa7
Yasin Tella <144742981+YasinTella@users.noreply.github.com>
2023-10-21 00:17:19 +0100

    Work on Json and ways to implement it
    Looked at how lab excersise worked and tried examples from lecture slides

---
+++ Json/Stringify-Example.js
@@ -0,0 +1,11 @@
+var json = {
+    Car: {
+    type: "Hybrid",
+    manufacturer: "KaluaKalua",
+    engine: "V8",
+    year: 2019,
+    mileage: 234
+    }
+};
+
+console.log(JSON.stringify(json));
